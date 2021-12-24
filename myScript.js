dddcafcc3a6c0bc1c7d9d96f6afa55dcdb977909
smit312 <smit.coder31@gmail.com>
2021-12-24 18:50:05 +0530

    TE5-T145 -learnt javascript basic

---
+++ myScript.js
@@ -0,0 +1,12 @@
+function para(){
+    document.getElementById("para1").innerHTML = "paragraph changed sucessfully !"
+}
+var x,y,z ;
+x= 5;
+y=6;
+z = x+y;
+document.write(z);
+
+var w = "John Doe";
+
+var w  = 0; 
